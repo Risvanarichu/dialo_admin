8cefe1be0d775ef040f20b557a66e79a9f507f9c
Shibin <gohack72@gmail.com>
2026-03-13 11:39:33 +0530

    pulling

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
-import 'package:dialo_admin/views/reportpage.dart';
+import 'package:dialo_admin/views/report/reportpage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'views/calls.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: ReportsPage(),
+      home: Calls(),
       debugShowCheckedModeBanner: false,
     );
   }
